707e1dbee6517a223cf68b10984c1f32091d0ffa
Tomas Di Bacco <100334543+Tdibacco17@users.noreply.github.com>
2022-07-18 09:57:25 -0300

    end

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -6,7 +6,7 @@ export default function CardDog({id, name, weight_min, weight_max, temperament, 
     return (
             <div className="cardDog">
                 <h3>{name}</h3>
-                <a href={`http://localhost:3000/dogs/${id}`}><img src={image} alt="img not found" width="200px" height="250px"/></a>
+                <a href={`/dogs/${id}`}><img src={image} alt="img not found" width="200px" height="250px"/></a>
                 <h5>Weight: {weight_min} - {weight_max}</h5>
                 <h5>Termperaments: {temperament}</h5>
             </div>
