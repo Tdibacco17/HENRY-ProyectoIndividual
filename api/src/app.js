b05f1a4a608ed66f55268c5c698e9210fb3bac3f
Tomas Di Bacco <100334543+Tdibacco17@users.noreply.github.com>
2022-09-01 09:53:51 -0300

    8

--- api/src/app.js
+++ api/src/app.js
@@ -9,27 +9,20 @@ require('./db.js');
 const server = express();
 
 server.name = 'API';
-whiteList = ["https://app-proyecto-individual.vercel.app/dogs", "https://pi-dogs-main-pearl.vercel.app"]
 
-server.use(cors({origin: whiteList}))
+
+server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-// server.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', "*"); // update to match the domain you will make the request from
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
-// server.use((req,res, next)=>{
-//   res.set('Access-Control-Allow-Origin',"http://localhost:6813");
-//   res.setHeader('Access-Control-Allow-Headers',"*");
-//   res.header('Access-Control-Allow-Credentials', true);
-//   next();
-// });
-
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', "*"); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 
 server.use('/', routes);
