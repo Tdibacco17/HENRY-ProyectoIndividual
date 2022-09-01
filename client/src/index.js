b79bd343957405128267c59d5aca6a3f762a7e68
Tomas Di Bacco <100334543+Tdibacco17@users.noreply.github.com>
2022-09-01 10:23:01 -0300

    falta cors

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3000";
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:6813";
 
 ReactDOM.render(
   <Provider store = {store}>
