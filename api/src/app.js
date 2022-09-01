8364cd460eb977bdec052b25a608374cb710d766
Tomas Di Bacco <100334543+Tdibacco17@users.noreply.github.com>
2022-09-01 09:12:07 -0300

    prueba2 cors

--- api/src/app.js
+++ api/src/app.js
@@ -10,18 +10,14 @@ const server = express();
 
 server.name = 'API';
 
-server.use(cors({
-  origin: "http://localhost:6813"
-}))
+server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', "*"); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+server.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
