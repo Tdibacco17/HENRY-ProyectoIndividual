0456cf4b6725ebc6dc73dc4f3d06583846aa657c
Tomas Di Bacco <100334543+Tdibacco17@users.noreply.github.com>
2022-09-01 09:33:19 -0300

    5

--- api/src/app.js
+++ api/src/app.js
@@ -10,7 +10,7 @@ const server = express();
 
 server.name = 'API';
 
-server.use(cors({origin: "http://localhost:6813"}))
+server.use(cors({origin: ["http://localhost:6813"]}))
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
