ef437b63ae1c1f59fc7a6ed577f95981a7d3b633
Tomas Di Bacco <100334543+Tdibacco17@users.noreply.github.com>
2022-09-29 11:58:09 -0300

    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from 'react-redux';
-import {store} from './store'
-import {BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store'
+import { BrowserRouter } from 'react-router-dom';
 import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 
-axios.defaults.baseURL = "http://localhost:3000"; // process.env.REACT_APP_API || 
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3000";
 
 ReactDOM.render(
-  <Provider store = {store}>
+  <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
         <App />
