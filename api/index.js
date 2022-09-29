bf563ff1023d9b6b47ee48e5c6f42f1cabe85f0b
Tomas Di Bacco <100334543+Tdibacco17@users.noreply.github.com>
2022-09-29 12:01:27 -0300

    port

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-const PORT = process.env.dogs || 3000;
+const PORT = process.env.PORT || 3000; 
 const HOST = process.env.HOST || "0.0.0.0";
 
 // Syncing all the models at once.
