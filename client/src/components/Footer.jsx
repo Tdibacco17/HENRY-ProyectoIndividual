70e3f5acb4dfbdb3b9f47add2a6dd08e241fd234
Tomas Di Bacco <100334543+Tdibacco17@users.noreply.github.com>
2023-01-25 11:34:52 -0300

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
             <img className="imgHenry" src={require("../img/logoHenry.png").default} alt=""/>
             <p> 
                 <img src={require("../img/LinkedinLogo.png").default} alt="" width="16px" height="16px"/>
-                <a href="https://www.linkedin.com/in/tomas-di-bacco-20a7a7240/" target="_blank" rel="noreferrer"> By Tomas Di Bacoo </a>
+                <a href="https://www.linkedin.com/in/tomas-di-bacco/" target="_blank" rel="noreferrer"> By Tomas Di Bacoo </a>
             </p>
             <img className="imgLogoDog" src={require("../img/dog.png").default} alt=""/>
             
