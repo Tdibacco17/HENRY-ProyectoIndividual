3d6b4f238e0b2271b6efb8cc75f9d73c105db983
Tomas Di Bacco <100334543+Tdibacco17@users.noreply.github.com>
2023-01-30 22:12:06 -0300

    font family nuevo sans serif

--- client/src/components/DetailComponent/DetailComponent.jsx
+++ client/src/components/DetailComponent/DetailComponent.jsx
@@ -16,13 +16,13 @@ export default function DetailComponent({ myDog, handleClick }) {
                         <div className="detailBody">
                             <div className="info_body">
                                 <h1>{myDog[0].name}</h1>
-                                <h4>Termperaments: {myDog[0].temperament}</h4>
-                                <h4>Height min: {myDog[0].height_min}</h4>
-                                <h4>Height max: {myDog[0].height_max}</h4>
-                                <h4>Weight min: {myDog[0].weight_min}kg</h4>
-                                <h4>Weight max: {myDog[0].weight_max}kg</h4>
-                                <h4>Life span min: {myDog[0].year_min} age</h4>
-                                <h4>Life span max: {myDog[0].year_max} age</h4>
+                                <h4>TEMPERAMENTS: {myDog[0].temperament} </h4>
+                                <h4>HEIGHT MIN: {myDog[0].height_min}</h4>
+                                <h4>HEIGHT MAX: {myDog[0].height_max}</h4>
+                                <h4>WEIGHT MIN: {myDog[0].weight_min}kg</h4>
+                                <h4>WEIGHT MAX: {myDog[0].weight_max}kg</h4>
+                                <h4>LIFE SPAN MIN: {myDog[0].year_min} age</h4>
+                                <h4>LIFE SPAN MAX: {myDog[0].year_max} age</h4>
                             </div>
                             <div className="info_btn">
                                 {
